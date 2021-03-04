2ffea23523974334e3813d0eb26e04152804b74b
Innti <fireinnti@gmail.com>
2021-03-03 18:04:34 -0600

    fixed json files

--- DiscordBotApp/GoogleCalendar.cs
+++ DiscordBotApp/GoogleCalendar.cs
@@ -99,7 +99,7 @@ namespace CalendarStyle
             {
                 // The file token.json stores the user's access and refresh tokens, and is created
                 // automatically when the authorization flow completes for the first time.
-                string credPath = "tokenCalendars.json";
+                string credPath = "tokenCalendar.json";
                 credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
                     GoogleClientSecrets.Load(stream).Secrets,
                     Scopes,
